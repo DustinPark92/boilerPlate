7476d01953ae3af4012c18579459f1e3836acd85
DustinPark92 <p4569zz@gmail.com>
2021-01-01 18:08:06 +0900

    비 밀 정 보 보 호

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const config = require('./config/key');
 const { User } = require("./models/User");
+
 
 
 
@@ -12,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://dustin:qudgh123@dustin.plarq.mongodb.net/dustin?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify : false
 }).then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
@@ -20,7 +22,7 @@ mongoose.connect('mongodb+srv://dustin:qudgh123@dustin.plarq.mongodb.net/dustin?
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+ 
 app.post('/register',(req, res) => {
     //회원 가입 할떄 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
@@ -36,8 +38,6 @@ app.post('/register',(req, res) => {
         })
     })
 })
-
-ㅌㅌㅌㅌㅌ
 
 //
 
